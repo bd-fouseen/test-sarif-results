df989f8e5888d27c4f3ceaab8714d9c6574a1909
bd-fouseen <fouseen@blackduck.com>
2025-07-30 12:03:07 +0100

    Update test-NO-01.ts

--- typescript/test-NO-01.ts
+++ typescript/test-NO-01.ts
@@ -1,15 +1,36 @@
 
 
-// https://github.com/wix/velo-external-db/blob/daf29885fcc29ddeca8f9ad20d6903245318ef76/scripts/provision/lib/cloud-providers/azure/azure_network.js#L17
-import { NetworkManagementClient } from '@azure/arm-network';
-import { DefaultAzureCredential } from '@azure/identity';
+// https://github.com/openapi-env-test/azure-sdk-for-js/blob/0541144368ee9adef989f07da783efd7fba75027/sdk/network/arm-network/samples-dev/virtualNetworksCreateOrUpdateSample.ts#L197
+import { VirtualNetwork, NetworkManagementClient } from "@azure/arm-network";
+import { DefaultAzureCredential } from "@azure/identity";
 
-function createVirtualNetworkInstance(resourceGroupName, virtualNetworkName) {
-    networkClient = new NetworkManagementClient(new DefaultAzureCredential(), "subscriptionId")
-    const parameters = {
-        location: this.region,
-        addressSpace: { addressPrefixes: ['10.0.0.0/16'] },
-        enableDdosProtection: true //#nodefect#ddos_protection_disabled_node_arm_network
-    };
-    const virtualNetwork = networkClient.virtualNetworks.beginCreateOrUpdate(resourceGroupName, virtualNetworkName, parameters)
+async function createVirtualNetworkWithServiceEndpointsAndServiceEndpointPolicy() {
+  const subscriptionId = "subid";
+  const resourceGroupName = "vnetTest";
+  const virtualNetworkName = "vnet1";
+  const parameters: VirtualNetwork = {
+    addressSpace: { addressPrefixes: ["10.0.0.0/16"] },
+    location: "eastus2euap",
+    enableDdosProtection: false, //#defect#ddos_protection_disabled_node_arm_network
+    subnets: [
+      {
+        name: "test-1",
+        addressPrefix: "10.0.0.0/16",
+        serviceEndpointPolicies: [
+          {
+            id:
+              "/subscriptions/subid/resourceGroups/vnetTest/providers/Microsoft.Network/serviceEndpointPolicies/ServiceEndpointPolicy1"
+          }
+        ],
+        serviceEndpoints: [{ service: "Microsoft.Storage" }]
+      }
+    ]
+  };
+  const credential = new DefaultAzureCredential();
+  const client = new NetworkManagementClient(credential, subscriptionId);
+  const result = await client.virtualNetworks.beginCreateOrUpdateAndWait(
+    resourceGroupName,
+    virtualNetworkName,
+    parameters
+  );
 }
