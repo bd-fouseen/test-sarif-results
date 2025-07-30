cc576758474c8e04be68d8f38be87d3a8407b5b4
bd-fouseen <fouseen@blackduck.com>
2025-07-30 12:03:48 +0100

    Update test-NO-01.ts

--- typescript/test-NO-01.ts
+++ typescript/test-NO-01.ts
@@ -4,33 +4,33 @@
 import { VirtualNetwork, NetworkManagementClient } from "@azure/arm-network";
 import { DefaultAzureCredential } from "@azure/identity";
 
-async function createVirtualNetworkWithServiceEndpointsAndServiceEndpointPolicy() {
-  const subscriptionId = "subid";
-  const resourceGroupName = "vnetTest";
-  const virtualNetworkName = "vnet1";
-  const parameters: VirtualNetwork = {
-    addressSpace: { addressPrefixes: ["10.0.0.0/16"] },
-    location: "eastus2euap",
-    enableDdosProtection: false, //#defect#ddos_protection_disabled_node_arm_network
-    subnets: [
-      {
-        name: "test-1",
-        addressPrefix: "10.0.0.0/16",
-        serviceEndpointPolicies: [
-          {
-            id:
-              "/subscriptions/subid/resourceGroups/vnetTest/providers/Microsoft.Network/serviceEndpointPolicies/ServiceEndpointPolicy1"
-          }
-        ],
-        serviceEndpoints: [{ service: "Microsoft.Storage" }]
-      }
-    ]
-  };
-  const credential = new DefaultAzureCredential();
-  const client = new NetworkManagementClient(credential, subscriptionId);
-  const result = await client.virtualNetworks.beginCreateOrUpdateAndWait(
-    resourceGroupName,
-    virtualNetworkName,
-    parameters
-  );
+async function createVirtualNetworkWithServiceEndpointsAndServiceEndpointPolicy_1() {
+    const subscriptionId = "subid";
+    const resourceGroupName = "vnetTest";
+    const virtualNetworkName = "vnet1";
+    const parameters: VirtualNetwork = {
+        addressSpace: { addressPrefixes: ["10.0.0.0/16"] },
+        location: "eastus2euap",
+        enableDdosProtection: true, //#nodefect#ddos_protection_disabled_node_arm_network
+        subnets: [
+            {
+                name: "test-1",
+                addressPrefix: "10.0.0.0/16",
+                serviceEndpointPolicies: [
+                    {
+                        id:
+                            "/subscriptions/subid/resourceGroups/vnetTest/providers/Microsoft.Network/serviceEndpointPolicies/ServiceEndpointPolicy1"
+                    }
+                ],
+                serviceEndpoints: [{ service: "Microsoft.Storage" }]
+            }
+        ]
+    };
+    const credential = new DefaultAzureCredential();
+    const client = new NetworkManagementClient(credential, subscriptionId);
+    const result = await client.virtualNetworks.beginCreateOrUpdateAndWait(
+        resourceGroupName,
+        virtualNetworkName,
+        parameters
+    );
 }
